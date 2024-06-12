1ef1d006e21459f32b0481013344d04637dbced2
ramirez823 <cdanielrv.823@gmail.com>
2024-06-12 14:40:42 -0600

    Semana 3 Proyecto inicial

---
+++ src/test/java/com/tienda_m/Tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_m.Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
